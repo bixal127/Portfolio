7e13ef0f81624f39e45dc96733b5163081439086
Dinesh Poudel <dineshp4297501@gmail.com>
2026-06-04 13:17:27 +0545

    4 June 2026

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/", // This should match your repository name exactly
+  base: '/',
   resolve: {
     extensions: ['.js', '.jsx'],
   },
